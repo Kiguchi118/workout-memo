6b430e0683b6f32ce204b916dc152bb1a31af3f2
Kiguchi118 <kiguchi118@gmail.com>
2020-11-23 23:54:03 +0900

    [add] テストユーザー & ログイン遷移先

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+##### テストユーザー #####
+User.create!(
+    email: "test@example.com",
+    password: "abc123ii",
+    height: 171.3,
+    sex: 0,
+ )
+ 
