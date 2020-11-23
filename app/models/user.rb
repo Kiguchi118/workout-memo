882ebf8f31a47138c8277258121f2beb64ec1fe4
Kiguchi118 <kiguchi118@gmail.com>
2020-11-23 15:29:10 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :sex, presence: true
+  validates :height, presence: true
+  enum sex: { male: 0, female: 1 }
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  root 'tops#index'
+  
+  get 'youtube/index'
+  devise_scope :user do
+    get "signup", to: "users/registrations#new", as: :new_user_registration
+    get 'login', to: 'users/sessions#new', as: :new_user_session
+    post 'login', to: 'users/sessions#create', as: :user_session
+    delete 'logout', to: 'users/sessions#destroy', as: :destroy_user_session
+  end
+
+  devise_for :users, skip: [:sessions],
+  controllers: {
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
+end
